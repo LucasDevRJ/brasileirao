116773e43c06b8e1ec1246429e8ad25ed0d3d004
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-07 16:44:50 -0300

    Verificação do número máximo de partidas

--- javaScript/index.js
+++ javaScript/index.js
@@ -7,14 +7,18 @@ function calculaPontuacao() {
 	var vitorias = parseInt(campoVitorias.value);
 	var empates = parseInt(campoEmpates.value);
 
-	//No Brasileirão, cada vitória é conquistado 3 pontos
-	//E cada empate é conquistado 1 ponto
-	var pontuacao = vitorias * 3 + empates;
+	if (vitorias + empates > 38) {
+		alert("O máximo são 38 partidas!");
+	} else {
+		//No Brasileirão, cada vitória é conquistado 3 pontos
+		//E cada empate é conquistado 1 ponto
+		var pontuacao = vitorias * 3 + empates;
 
-	//No Brasileirão tem-se 38 partidas no total
-	var derrotas = ((vitorias + empates) - 38) * -1;
+		//No Brasileirão tem-se 38 partidas no total
+		var derrotas = ((vitorias + empates) - 38) * -1;
 
-	exibeRespostas(time, vitorias, empates, derrotas, pontuacao);
+		exibeRespostas(time, vitorias, empates, derrotas, pontuacao);
+	}
 }
 
 function exibeRespostas(time, vitorias, empates, derrotas, pontuacao) {
