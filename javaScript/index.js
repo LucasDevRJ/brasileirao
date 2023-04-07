7bec9222f18e521d9de07d143eccd2f8eedf59f6
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-07 16:16:00 -0300

    Função que calcula a pontuação e botão para aciona-la

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,7 +1,16 @@
-var campoTime = document.getElementById("time");
-var campoVitorias = document.getElementById("vitorias");
-var campoEmpates = document.getElementById("empates");
+function calculaPontuacao() {
+	var campoTime = document.getElementById("time");
+	var campoVitorias = document.getElementById("vitorias");
+	var campoEmpates = document.getElementById("empates");
 
-console.log(campoTime);
-console.log(campoVitorias);
-console.log(campoEmpates);
+	var time = campoTime.value;
+	var vitorias = campoVitorias.value;
+	var empates = campoEmpates.value;
+
+	console.log(time);
+	console.log(vitorias);
+	console.log(empates);
+}
+
+var botao = document.querySelector("button");
+botao.addEventListener("click", calculaPontuacao);
