8345bc776a96a63dffed754a99f967e954ba7219
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-07 16:09:00 -0300

    Pegando todos os campos

---
+++ javaScript/index.js
@@ -0,0 +1,7 @@
+var campoTime = document.getElementById("time");
+var campoVitorias = document.getElementById("vitorias");
+var campoEmpates = document.getElementById("empates");
+
+console.log(campoTime);
+console.log(campoVitorias);
+console.log(campoEmpates);
