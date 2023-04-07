5dcd3df7f3c9a0304a9e092ade767feb6cc01ad9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-07 16:28:10 -0300

    Comentários explicando a lógica

--- javaScript/index.js
+++ javaScript/index.js
@@ -3,13 +3,16 @@ function calculaPontuacao() {
 	var campoVitorias = document.getElementById("vitorias");
 	var campoEmpates = document.getElementById("empates");
 
-	var time = campoTime.value;
-	var vitorias = campoVitorias.value;
-	var empates = campoEmpates.value;
+	var time = parseInt(campoTime.value);
+	var vitorias = parseInt(campoVitorias.value);
+	var empates = parseInt(campoEmpates.value);
 
-	console.log(time);
-	console.log(vitorias);
-	console.log(empates);
+	//No Brasileirão, cada vitória é conquistado 3 pontos
+	//E cada empate é conquistado 1 ponto
+	var pontuacao = vitorias * 3 + empates;
+
+	//No Brasileirão tem-se 38 partidas no total
+	var derrotas = ((vitorias + empates) - 38) * -1;
 }
 
 var botao = document.querySelector("button");
